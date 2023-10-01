5115e26de0dbe73f0aea9b3fcc6110c4f9eea7a6
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-01 14:54:33 +0100

    Render user data from state object

--- contact-card/src/App.jsx
+++ contact-card/src/App.jsx
@@ -4,19 +4,26 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [contact, setContact] = useState({
+    firstName: 'Jane',
+    image: 'user.png',
+    lastName: 'Doe',
+    phone: '+234(0) 806 484 4004',
+    email: 'itsmyrealname@example.com',
+    isFavorite: false
+  })
 
   return (
     <div className='container'>
       <main>
         <article className="card">
-            <img src="/images/user.png" alt="" className="card--image" />
+            <img src={`/images/${contact.image}`} alt="" className="card--image" />
 
             <div className="card--info">
                 <img src={`/images/star-empty.png`} alt="" className="card--star" />
-                <h1 className="card--name">John Doe</h1>
-                <p className="card--phone">+234(0) 806 484 4004</p>
-                <p className="card--email">itsmyrealname@example.com</p>
+                <h1 className="card--name">{contact.firstName} {contact.lastName}</h1>
+                <p className="card--phone">{contact.phone}</p>
+                <p className="card--email">{contact.email}</p>
             </div>
         </article>
       </main>
