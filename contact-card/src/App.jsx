648298187cc319c5a0a04deb370b1347544e2711
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-01 12:08:18 +0100

    Create contact card markup

--- contact-card/src/App.jsx
+++ contact-card/src/App.jsx
@@ -8,7 +8,18 @@ function App() {
 
   return (
     <div className='container'>
-      
+      <main>
+        <article className="card">
+            <img src="" alt="" className="card--image" />
+
+            <div className="card--info">
+                <img src="" alt="" className="card--star" />
+                <h2 className="card--name">John Doe</h2>
+                <p className="card--phone">+234(0) 806 484 4004</p>
+                <p className="card--email">itsmyrealname@example.com</p>
+            </div>
+        </article>
+      </main>
     </div>
   )
 }
