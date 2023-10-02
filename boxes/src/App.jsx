f36dc70c5d9d58b9e208ec8aac267592ad1adb80
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-02 08:16:08 +0100

    Toggle a box bg-color when clicked

--- boxes/src/App.jsx
+++ boxes/src/App.jsx
@@ -6,7 +6,15 @@ import Box from './components/Box';
 function App() {
     const [boxArray, setBoxArray] = useState(boxes);
 
-    const boxJsx = boxArray.map(box => <Box key={box.id} square={box} />);
+    const boxJsx = boxArray.map(box => <Box key={box.id} square={box} toggleFunc={toggleOn} />);
+
+    function toggleOn(id) {
+        setBoxArray(prevArray => {
+            return prevArray.map(box => {
+                return box.id === id ? {...box, on: !box.on} : box
+            })
+        })
+    }
 
     return (
         <div className='container'>
--- boxes/src/components/Box.jsx
+++ boxes/src/components/Box.jsx
@@ -1,11 +1,9 @@
 export default function Box(props) {
-    console.log(props.square);
-
     const styles = {
         backgroundColor: props.square.on ? '#222222' : 'transparent'
     }
 
     return (
-        <div style={styles} className='box'></div>
+        <div style={styles} onClick={() => props.toggleFunc(props.square.id)} className='box'></div>
     )
 }
