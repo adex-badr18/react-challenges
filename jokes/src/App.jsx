3dbb2f2e8b1bd72c79fb4f266ab971b9f247cfaa
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-02 17:25:06 +0100

    Render jokes and punchline on page

--- jokes/src/App.jsx
+++ jokes/src/App.jsx
@@ -1,13 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import jokesData from './jokesData';
 
 function App() {
-    
+    const jokesJsx = jokesData.map(joke => {
+        return (
+            <div className="joke--container" key={joke.id}>
+                <h2 className="setup">{joke.setup}</h2>
+                <p className="punchline">{joke.punchline}</p>
+                <hr />
+            </div>
+        )
+    })
+
     return (
         <div className='container'>
-            
+            {jokesJsx}
         </div>
     )
 }
