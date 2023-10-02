90799a5219b7b3bf393d3002bab7a55d1f3b9b53
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-02 17:44:29 +0100

    Toggle punchline display on button click

--- jokes/src/components/Joke.jsx
+++ jokes/src/components/Joke.jsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
+
 export default function Joke(props) {
     const joke = props.joke;
+    const [isShown, setIsShown] = useState(false);
+
+    function toggleShown() {
+        setIsShown(prevState => !prevState);
+    }
 
     return (
         <div className="joke--container">
             {joke.setup && <h2 className="setup">{joke.setup}</h2>}
-            <p className="punchline">{joke.punchline}</p>
+            {isShown && <p className="punchline">{joke.punchline}</p>}
+            <button className="toggle-btn" onClick={toggleShown}>Show Punchline</button>
             <hr />
         </div>
     )
