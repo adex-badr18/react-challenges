c1db7d4b9fc5a2f88c1b05727aeb54b904bef268
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-02 17:41:07 +0100

    Render joke setup only if it exits

--- jokes/src/components/Joke.jsx
+++ jokes/src/components/Joke.jsx
@@ -1,8 +1,9 @@
 export default function Joke(props) {
     const joke = props.joke;
+
     return (
         <div className="joke--container">
-            <h2 className="setup">{joke.setup}</h2>
+            {joke.setup && <h2 className="setup">{joke.setup}</h2>}
             <p className="punchline">{joke.punchline}</p>
             <hr />
         </div>
